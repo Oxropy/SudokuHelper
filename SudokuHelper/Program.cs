bae084b34a2f47939baba353bd07afd58dfbb1e5
Alexander Pollich <oxropy-lex@t-online.de>
2018-04-30 23:32:58 +0200

    Remove Group and Field class and change structure

--- SudokuHelper/Program.cs
+++ SudokuHelper/Program.cs
@@ -11,35 +11,41 @@ namespace SudokuHelper
     {
         static void Main(string[] args)
         {
+            int highestValue = 9;
             Console.WriteLine("Sudoku path:");
             string pathInput = Console.ReadLine();
-            var values = ImportSudoku(pathInput);
+            var values = ImportSudoku(pathInput, highestValue);
 
             if (values.Length > 0)
             {
-                Sudoku sudoku = new Sudoku(values, SudokuHandler.GetDefaultSquares());
-                SudokuHandler.PrintSudoku(sudoku);
+                Sudoku.PrintSudoku(values, highestValue);
 
+                var groups = Sudoku.GetDefaultGroups();
                 Stopwatch sw = Stopwatch.StartNew();
-                SudokuHandler.SolveSudoku(sudoku);
+                var solved = Sudoku.Solve(groups, values);
                 sw.Stop();
-                Console.WriteLine("Time: {0}ms, {1}ticks", sw.ElapsedMilliseconds, sw.ElapsedTicks);
-                SudokuHandler.PrintSudoku(sudoku);
+                Console.WriteLine("Time: {0}ms", sw.ElapsedMilliseconds);
+                if (solved.IsSome)
+                {
+                    Sudoku.PrintSudoku(solved.Value, highestValue); 
+                }
             }
             Console.ReadKey();
         }
 
-        private static int[][] ImportSudoku(string path)
+        private static ImmMap<(int row, int column), ImmList<int>> ImportSudoku(string path, int highestValue)
         {
-            var values = new List<int[]>();
+            Dictionary<(int row, int column), ImmList<int>> fields = new Dictionary<(int row, int column), ImmList<int>>();
             try
             {
                 using (StreamReader r = File.OpenText(path))
                 {
+                    int row = 0;
                     string line;
                     while ((line = r.ReadLine()) != null)
                     {
-                        values.Add(GetIntValuesOutOfStringLine(line));
+                        SetRowFieldsOutOfStringLine(fields, line, row, highestValue);
+                        row++;
                     }
                 }
             }
@@ -47,32 +53,26 @@ namespace SudokuHelper
             {
                 Console.WriteLine(string.Format("Sudoku import error: {0}", ex.Message));
             }
-            return values.ToArray();
+            return fields.ToImmMap();
         }
 
-        private static int[] GetIntValuesOutOfStringLine(string line)
+        private static void SetRowFieldsOutOfStringLine(Dictionary<(int row, int column), ImmList<int>> fields, string line, int row, int highestValue)
         {
-            var values = new List<int>();
             for (int i = 0; i < line.Length; i++)
             {
                 if (int.TryParse(line[i].ToString(), out int value))
                 {
-                    values.Add(value);
+                    fields.Add((row: row, column: i), value == 0 ? Enumerable.Range(1, highestValue).ToImmList() : ImmList.Of(value));
                 }
-
             }
-            return values.ToArray();
         }
     }
 
-    static class SudokuHandler
+    static class Sudoku
     {
-        public static List<List<Tuple<int, int>>> GetDefaultSquares()
+        public static ImmList<ImmList<Tuple<int, int>>> GetDefaultGroups()
         {
-            List<List<Tuple<int, int>>> groups = new List<List<Tuple<int, int>>>();
-
-            var square = new List<Tuple<int, int>>()
-            {
+            var square1 = ImmList.Of(
                 new Tuple<int,int>(0,0),
                 new Tuple<int,int>(0,1),
                 new Tuple<int,int>(0,2),
@@ -82,11 +82,9 @@ namespace SudokuHelper
                 new Tuple<int,int>(2,1),
                 new Tuple<int,int>(2,2),
                 new Tuple<int,int>(2,3)
-            };
-            groups.Add(square);
-
-            square = new List<Tuple<int, int>>()
-            {
+            );
+            
+            var square2 = ImmList.Of(
                 new Tuple<int,int>(4,0),
                 new Tuple<int,int>(4,1),
                 new Tuple<int,int>(4,2),
@@ -96,11 +94,9 @@ namespace SudokuHelper
                 new Tuple<int,int>(6,1),
                 new Tuple<int,int>(6,2),
                 new Tuple<int,int>(6,3)
-            };
-            groups.Add(square);
-
-            square = new List<Tuple<int, int>>()
-            {
+            );
+            
+            var square3 = ImmList.Of(
                 new Tuple<int,int>(7,0),
                 new Tuple<int,int>(7,1),
                 new Tuple<int,int>(7,2),
@@ -110,11 +106,9 @@ namespace SudokuHelper
                 new Tuple<int,int>(9,1),
                 new Tuple<int,int>(9,2),
                 new Tuple<int,int>(9,3)
-            };
-            groups.Add(square);
-
-            square = new List<Tuple<int, int>>()
-            {
+            );
+            
+            var square4 = ImmList.Of(
                 new Tuple<int,int>(0,4),
                 new Tuple<int,int>(0,5),
                 new Tuple<int,int>(0,6),
@@ -124,11 +118,9 @@ namespace SudokuHelper
                 new Tuple<int,int>(2,4),
                 new Tuple<int,int>(2,5),
                 new Tuple<int,int>(2,6)
-            };
-            groups.Add(square);
-
-            square = new List<Tuple<int, int>>()
-            {
+            );
+            
+            var square5 = ImmList.Of(
                 new Tuple<int,int>(3,4),
                 new Tuple<int,int>(3,5),
                 new Tuple<int,int>(3,6),
@@ -138,11 +130,9 @@ namespace SudokuHelper
                 new Tuple<int,int>(5,4),
                 new Tuple<int,int>(5,5),
                 new Tuple<int,int>(5,6)
-            };
-            groups.Add(square);
-
-            square = new List<Tuple<int, int>>()
-            {
+            );
+            
+            var square6 = ImmList.Of(
                 new Tuple<int,int>(6,4),
                 new Tuple<int,int>(6,5),
                 new Tuple<int,int>(6,6),
@@ -152,11 +142,9 @@ namespace SudokuHelper
                 new Tuple<int,int>(8,4),
                 new Tuple<int,int>(8,5),
                 new Tuple<int,int>(8,6)
-            };
-            groups.Add(square);
+            );
 
-            square = new List<Tuple<int, int>>()
-            {
+            var square7 = ImmList.Of(
                 new Tuple<int,int>(0,7),
                 new Tuple<int,int>(0,8),
                 new Tuple<int,int>(0,9),
@@ -166,11 +154,9 @@ namespace SudokuHelper
                 new Tuple<int,int>(2,7),
                 new Tuple<int,int>(2,8),
                 new Tuple<int,int>(2,9)
-            };
-            groups.Add(square);
-
-            square = new List<Tuple<int, int>>()
-            {
+            );
+            
+            var square8 = ImmList.Of(
                 new Tuple<int,int>(4,7),
                 new Tuple<int,int>(4,8),
                 new Tuple<int,int>(4,9),
@@ -180,11 +166,9 @@ namespace SudokuHelper
                 new Tuple<int,int>(6,7),
                 new Tuple<int,int>(6,8),
                 new Tuple<int,int>(6,9)
-            };
-            groups.Add(square);
-
-            square = new List<Tuple<int, int>>()
-            {
+            );
+            
+            var square9 = ImmList.Of(
                 new Tuple<int,int>(7,7),
                 new Tuple<int,int>(7,8),
                 new Tuple<int,int>(7,9),
@@ -194,13 +178,12 @@ namespace SudokuHelper
                 new Tuple<int,int>(9,7),
                 new Tuple<int,int>(9,8),
                 new Tuple<int,int>(9,9)
-            };
-            groups.Add(square);
+            );
 
+            var groups = new List<ImmList<Tuple<int,int>>>();
             for (int i = 0; i < 9; i++)
             {
-                var row = new List<Tuple<int, int>>()
-                {
+                var row = ImmList.Of(
                     new Tuple<int,int>(i,0),
                     new Tuple<int,int>(i,1),
                     new Tuple<int,int>(i,2),
@@ -210,11 +193,10 @@ namespace SudokuHelper
                     new Tuple<int,int>(i,6),
                     new Tuple<int,int>(i,7),
                     new Tuple<int,int>(i,8)
-                };
+                );
                 groups.Add(row);
 
-                var column = new List<Tuple<int, int>>()
-                {
+                var column = ImmList.Of(
                     new Tuple<int,int>(0,i),
                     new Tuple<int,int>(1,i),
                     new Tuple<int,int>(2,i),
@@ -224,139 +206,39 @@ namespace SudokuHelper
                     new Tuple<int,int>(6,i),
                     new Tuple<int,int>(7,i),
                     new Tuple<int,int>(8,i)
-                };
+                );
                 groups.Add(column);
             }
 
-            return groups;
+            return ImmList.Of(square1, square2, square3, square4, square5, square6, square7, square8, square9).AddLastRange(groups);
         }
 
-        public static void SolveSudoku(Sudoku sudoku)
+        public static Optional<ImmMap<(int row, int column), ImmList<int>>> Solve(ImmList<ImmList<Tuple<int, int>>> groups, ImmMap<(int row, int column), ImmList<int>> fields)
         {
-            bool complete = false;
-            while (!complete)
-            {
-                List<Group> groups = new List<Group>();
-                complete = true;
-                foreach (var group in sudoku.Groups)
-                {
-                    var newFields = SolveGroup(group.Fields, group.Fields.Where(g => g.PossibleValues.Length == 1).Select(g => g.PossibleValues[0]));
-                    var newGroup = new Group(newFields, Sudoku.IsGroupComplete(newFields));
-                    
-                    if (!newGroup.Complete)
-                    {
-                        complete = false;
-                    }
-
-                    groups.Add(newGroup);
-                }
-                sudoku.Groups = groups;
-            }
+            return Optional.None;
         }
 
-        public static void PrintSudoku(Sudoku sudoku)
+        public static void PrintSudoku(ImmMap<(int row, int column), ImmList<int>> fields, int highestValue)
         {
-            IEnumerable<Field> fields = sudoku.Groups.SelectMany(g => g.Fields).Distinct();
-
-            for (int i = 0; i < sudoku.Groups.Count; i++)
+            for (int i = 0; i < highestValue; i++)
             {
-                IEnumerable<Field> row = fields.Where(f => f.Row == i).OrderBy(f => f.Column);
+                var row = fields.Where(f => f.Key.row == i).OrderBy(f => f.Key.column);
                 foreach (var field in row)
                 {
-                    int value = Sudoku.GetValueOrDefault(field);
-                    Console.Write(value);
+                    Console.Write(GetValueOrDefault(field.Value));
                 }
                 Console.WriteLine();
             }
         }
 
-        private static ImmList<Field> SolveGroup(ImmList<Field> group, ImmList<int> fixFields)
+        public static int GetValueOrDefault(ImmList<int> field)
         {
-            var field = group.First;
-            int value = Sudoku.GetValueOrDefault(field);
-            var newFields = group.RemoveFirst();
-            if (newFields.Length > 0)
-            {
-                var newGroup = SolveGroup(newFields, value == 0 ? fixFields : fixFields.AddLast(value)); 
-            }
-
-            return group.AddFirst(new Field(field.Row, field.Column, Sudoku.RemoveFixValuesFromPossibleValues(field.PossibleValues, fixFields)));
-        }
-    }
-
-    class Sudoku
-    {
-        public List<Group> Groups;
-
-        public Sudoku(int[][] values, List<List<Tuple<int, int>>> groups)
-        {
-            var fields = new List<Field>();
-            for (int i = 0; i < values.Length; i++)
-            {
-                for (int j = 0; j < values[i].Length; j++)
-                {
-                    int value = values[i][j];
-                    fields.Add(value == 0 ? new Field(i, j, Enumerable.Range(1, values.Length).ToImmList()) : new Field(i, j, ImmList.Of(value)));
-                }
-            }
-
-            Groups = new List<Group>();
-            foreach (var group in groups)
-            {
-                var fieldList = new List<Field>();
-                foreach (var field in fields)
-                {
-                    foreach (var position in group)
-                    {
-                        if (position.Item1 == field.Row && position.Item2 == field.Column)
-                        {
-                            fieldList.Add(field);
-                        }
-                    }
-                }
-                Groups.Add(new Group(fieldList));
-            }
-        }
-
-        public static bool IsGroupComplete(ImmList<Field> group)
-        {
-            return group.All(f => f.PossibleValues.Count() == 1);
-        }
-
-        public static int GetValueOrDefault(Field field)
-        {
-            return field.PossibleValues.Length == 1 ? field.PossibleValues[0] : 0;
+            return field.Length == 1 ? field[0] : 0;
         }
 
         public static ImmList<int> RemoveFixValuesFromPossibleValues(ImmList<int> values, ImmList<int> fixValues)
         {
             return values.Where(v => !fixValues.Contains(v)).ToImmList();
-        }
-    }
-
-    struct Group
-    {
-        public readonly ImmList<Field> Fields;
-        public readonly bool Complete;
-
-        public Group(IEnumerable<Field> fields, bool complete = false)
-        {
-            Fields = fields.ToImmList();
-            Complete = complete;
-        }
-    }
-
-    struct Field
-    {
-        public readonly int Row;
-        public readonly int Column;
-        public readonly ImmList<int> PossibleValues;
-
-        public Field(int row, int column, ImmList<int> possibleValues)
-        {
-            Row = row;
-            Column = column;
-            PossibleValues = possibleValues;
         }
     }
 }
